df60e18ef6517745279d7cf034a1eeec043a45b0
Olav Hobberstad <colddimension@gmail.com>
2017-09-27 09:40:30 +0200

    added db helpers

--- app/app.py
+++ app/app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import mysql.connector
 
 app = Flask(__name__)
 
 # Flask configuration parameters #
+# TODO set database vars
+app.config["DATABASE_USER"] = ""
+app.config["DATABASE_PASSWORD"] = ""
+app.config["DATABASE_DB"] = ""
+app.config["DATABASE_HOST"] = ""
 app.config['debug'] = True  # Testing only
 app.secret_key = 'hella secret'
+
+
+# database functions
+def get_db():
+    if not hasattr(g, "_database"):
+        g.db = mysql.connector.connect(host=app.config["DATABASE_HOST"], user=app.config["DATABASE_USER"],
+                                       password=app.config["DATABASE_PASSWORD"], database=app.config["DATABASE_DB"])
+    return g.db
+
+
+@app.teardown_appcontext
+def teardown_db(error):
+    """Closes the database at the end of the request."""
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 # TODO everything
