fd1212eea236e7f6d730ec4c9291e17b51bfbbd9
Olav Hobberstad <colddimension@gmail.com>
2017-10-04 17:04:22 +0200

    logging rework, error handling

--- app/app.py
+++ app/app.py
@@ -13,7 +13,6 @@ in the GitHub repository README.md
 """
 import logging
 import os
-import errno
 from flask import Flask, render_template, g, session, request, url_for, redirect
 from flask_mobility import Mobility
 from flask_mobility.decorators import mobile_template
@@ -45,33 +44,21 @@ def load_user(user_id):
 
 
 # TODO implement necessary loggers
-# Hold off on this until we have more than one python file to work with to avoid having to rework logging
-loggers = ['main']  # loggers to init
-
-
-def setup_logging(log_name, log_file, log_level=logging.INFO):
-    """
-    :param log_name: String, descriptive name of log
-    :param log_file: String, a path to .log file to write to
-    :param log_level: logging.<PARAM>, INFO, DEBUG or WARNING, level of logging
-    :return: log object, not necessary to use the returned object,
-            but useful for storing logs in a list or dict for easier access
-    """
-
-    try:  # makes log directory if none exist
-        os.makedirs('{}/logs'.format(os.getcwd()))
-        print('Log directory created.')
-    except OSError as e:
-        if e.errno is not errno.EEXIST:
-            raise
-
-    log = logging.getLogger(log_name)
-    log.setLevel(log_level)
-    handler = logging.FileHandler(filename=log_file, encoding='utf-8', mode='a')
-    fmt = logging.Formatter('[%(asctime)s] :%(levelname)s: %(message)s', datefmt='%H:%M:%S')
-    handler.setFormatter(fmt)
-    log.addHandler(handler)
-    return log
+def setup_logging():
+    try:
+        lg = logging.getLogger(__name__)
+        lg.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename='calendarize.log', encoding='utf-8', mode='a')
+        fmt = logging.Formatter('[%(asctime)s]:%(module)s:%(levelname)s: %(message)s', datefmt='%H:%M:%S')
+        handler.setFormatter(fmt)
+        lg.addHandler(handler)
+        return lg
+    except PermissionError as e:
+        print('{}\n'
+              'WARNING: Logging not enabled.\n'
+              'If you get this error, change your IDE working directory.\n'
+              'The application will still work, but nothing will be logged.'.format(e))
+        # In PyCharm, go to Run>Edit Configuration to set the working directory to the calendarize folder.
 
 
 def end_logging(log):
@@ -84,6 +71,11 @@ def end_logging(log):
 def request_data(req):
     res = '{} requested by {}'.format(req.url, req.remote_addr)
     return res
+
+
+def log():
+    if logger:
+        logger.info(request_data(request))
 
 
 def get_user_id():
@@ -103,7 +95,7 @@ def get_user_id():
 @mobile_template('/{mobile/}index.html')
 # @login_required
 def index(template):
-    lg.info(request_data(request))
+    log()
     # TODO fetch user data
     return render_template(template)
 
@@ -111,6 +103,7 @@ def index(template):
 @app.route('/view/<calendar_id>')
 @mobile_template('{mobile/}calendar.html')
 def view(template, calendar_id):
+    log()
     # TODO check if calendar exists and if the user has permission to view it
     # TODO create template and call render
     return render_template(template)
@@ -120,6 +113,7 @@ def view(template, calendar_id):
 @mobile_template('{mobile/}template.html')
 @login_required
 def settings(template):
+    log()
     # TODO load user's settings, then render a template with the settings
     return render_template(template)
 
@@ -162,7 +156,7 @@ def delete_event():
 @app.route('/delete_calendar', methods=['POST'])
 @login_required
 def delete_cal():
-    user = session['user']['id']
+    user = get_user_id()
     cal = request.form.get('calendar_id', None)
     if cal:
         # TODO
@@ -175,12 +169,8 @@ def delete_cal():
 
 if __name__ == '__main__':
 
-    active_loggers = []
-    for log in loggers:
-        lg = setup_logging(log, 'logs/{}.log'.format(log))
-        active_loggers.append(lg)
+    logger = setup_logging()
 
     app.run()
 
-    for log in active_loggers:
-        end_logging(log)
+    end_logging(logger)
