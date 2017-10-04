e82b3c3714087be8ca3515258a5cc5259d098667
Ivica Kostric <ivica_kostric@yahoo.com>
2017-10-04 10:16:17 +0200

    Revert "Merge branch 'dev' of https://github.com/dat210a/calendarize into dev"
    This reverts commit b829fe443d768c21c926551e7d691829d82b8098, reversing
    changes made to 46d0ab81f0e7f867e7027428291490ee4bbaf58c.

--- app/app.py
+++ app/app.py
@@ -1,25 +1,10 @@
-"""
-calendarize Flask application file
-
-The code for this project loosely follows PEP 8 style guidelines.
-You can read about them here: https://www.python.org/dev/peps/pep-0008/
-Any code that does not comply will be modified to do so if possible.
-If you're using PyCharm, it will have built-in PEP8 linting,
-and similar packages should be available for other editors.
-
-Links to documentation for the packages used can be found
-in the GitHub repository README.md
-
-"""
-import logging
-import os
-import errno
 from flask import Flask, render_template, g, session, request, url_for, redirect
 from flask_mobility import Mobility
 from flask_mobility.decorators import mobile_template
 from classes.user import User
 from flask_login import *
 from funcs.logIn import hash_password
+import logging
 
 app = Flask(__name__)
 Mobility(app)
@@ -32,21 +17,19 @@ app.config["DATABASE_DB"] = ""
 app.config["DATABASE_HOST"] = ""
 app.config['debug'] = True  # Testing only
 app.secret_key = 'hella secret'
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 hash_password("password")
 
+# TODO implement necessary loggers
+# Hold off on this until we have more than one python file to work with to avoid having to rework logging
+loggers = {}  # dict of loggers to init
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get_id(user_id)
-
-
-# TODO implement necessary loggers
-# Hold off on this until we have more than one python file to work with to avoid having to rework logging
-loggers = ['main']  # loggers to init
 
 
 def setup_logging(log_name, log_file, log_level=logging.INFO):
@@ -57,14 +40,6 @@ def setup_logging(log_name, log_file, log_level=logging.INFO):
     :return: log object, not necessary to use the returned object,
             but useful for storing logs in a list or dict for easier access
     """
-
-    try:  # makes log directory if none exist
-        os.makedirs('{}/logs'.format(os.getcwd()))
-        print('Log directory created.')
-    except OSError as e:
-        if e.errno is not errno.EEXIST:
-            raise
-
     log = logging.getLogger(log_name)
     log.setLevel(log_level)
     handler = logging.FileHandler(filename=log_file, encoding='utf-8', mode='a')
@@ -81,17 +56,6 @@ def end_logging(log):
         log.removeHandler(hdlr)
 
 
-def request_data(req):
-    res = '{} requested by {}'.format(req.url, req.remote_addr)
-    return res
-
-
-def get_user_id():
-    if 'user' in session:
-        return session['user']['id']
-    return None
-
-
 ##################################################################
 # Some of the routes below might warrant moving out and
 # into separate files, but until the scope of the operations
@@ -101,9 +65,8 @@ def get_user_id():
 
 @app.route('/')
 @mobile_template('/{mobile/}index.html')
-# @login_required
+@login_required
 def index(template):
-    lg.info(request_data(request))
     # TODO fetch user data
     return render_template(template)
 
@@ -140,22 +103,10 @@ def save_settings():
 @login_required
 def delete_user():
     # TODO rewrite if necessary when login functionality is implemented
-    req_user = get_user_id()
+    req_user = session['user']['id']
     del_user = request.form.get('user_id', None)
     if del_user and req_user == del_user:  # ensures only the user can delete themselves
         # TODO mark user as deleted in the database
-        pass
-
-
-@app.route('/delete_event', methods=['POST'])
-@login_required
-def delete_event():
-    user = get_user_id()
-    event = request.form.get('event_id', None)
-    if event:
-        # TODO
-        # Make db request to check if user has permission to delete this event
-        # Set event delete flag in db if yes
         pass
 
 
@@ -177,7 +128,7 @@ if __name__ == '__main__':
 
     active_loggers = []
     for log in loggers:
-        lg = setup_logging(log, 'logs/{}.log'.format(log))
+        lg = setup_logging(log, '{}.log'.format(log))
         active_loggers.append(lg)
 
     app.run()
