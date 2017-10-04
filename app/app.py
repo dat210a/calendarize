2c6595647dab9c4bd1c8f8b9b9cc4980c7d12ad7
Ivica Kostric <ivica_kostric@yahoo.com>
2017-10-04 17:39:42 +0200

    Revert "Merge branch 'dev' of https://github.com/dat210a/calendarize into dev"
    This reverts commit 174a402d69dcda5e2c68ed8cbd1b5ac158a82c88, reversing
    changes made to 1ee0be2ddb54b936f4040df8286e9a6775870212.

--- app/app.py
+++ app/app.py
@@ -1,24 +1,10 @@
-"""
-calendarize Flask application file
-
-The code for this project loosely follows PEP 8 style guidelines.
-You can read about them here: https://www.python.org/dev/peps/pep-0008/
-Any code that does not comply will be modified to do so if possible.
-If you're using PyCharm, it will have built-in PEP8 linting,
-and similar packages should be available for other editors.
-
-Links to documentation for the packages used can be found
-in the GitHub repository README.md
-
-"""
-import logging
-import os
 from flask import Flask, render_template, g, session, request, url_for, redirect
 from flask_mobility import Mobility
 from flask_mobility.decorators import mobile_template
 from classes.user import User
 from flask_login import *
 from funcs.logIn import hash_password
+import logging
 
 app = Flask(__name__)
 Mobility(app)
@@ -31,11 +17,14 @@ app.config["DATABASE_DB"] = ""
 app.config["DATABASE_HOST"] = ""
 app.config['debug'] = True  # Testing only
 app.secret_key = 'hella secret'
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 hash_password("password")
+
+# TODO implement necessary loggers
+# Hold off on this until we have more than one python file to work with to avoid having to rework logging
+loggers = {}  # dict of loggers to init
 
 
 @login_manager.user_loader
@@ -43,22 +32,21 @@ def load_user(user_id):
     return User.get_id(user_id)
 
 
-# TODO implement necessary loggers
-def setup_logging():
-    try:
-        lg = logging.getLogger(__name__)
-        lg.setLevel(logging.INFO)
-        handler = logging.FileHandler(filename='calendarize.log', encoding='utf-8', mode='a')
-        fmt = logging.Formatter('[%(asctime)s]:%(module)s:%(levelname)s: %(message)s', datefmt='%H:%M:%S')
-        handler.setFormatter(fmt)
-        lg.addHandler(handler)
-        return lg
-    except PermissionError as e:
-        print('{}\n'
-              'WARNING: Logging not enabled.\n'
-              'If you get this error, change your IDE working directory.\n'
-              'The application will still work, but nothing will be logged.'.format(e))
-        # In PyCharm, go to Run>Edit Configuration to set the working directory to the calendarize folder.
+def setup_logging(log_name, log_file, log_level=logging.INFO):
+    """
+    :param log_name: String, descriptive name of log
+    :param log_file: String, a path to .log file to write to
+    :param log_level: logging.<PARAM>, INFO, DEBUG or WARNING, level of logging
+    :return: log object, not necessary to use the returned object,
+            but useful for storing logs in a list or dict for easier access
+    """
+    log = logging.getLogger(log_name)
+    log.setLevel(log_level)
+    handler = logging.FileHandler(filename=log_file, encoding='utf-8', mode='a')
+    fmt = logging.Formatter('[%(asctime)s] :%(levelname)s: %(message)s', datefmt='%H:%M:%S')
+    handler.setFormatter(fmt)
+    log.addHandler(handler)
+    return log
 
 
 def end_logging(log):
@@ -66,22 +54,6 @@ def end_logging(log):
     for hdlr in handlers:
         hdlr.close()
         log.removeHandler(hdlr)
-
-
-def request_data(req):
-    res = '{} requested by {}'.format(req.url, req.remote_addr)
-    return res
-
-
-def log():
-    if logger:
-        logger.info(request_data(request))
-
-
-def get_user_id():
-    if 'user' in session:
-        return session['user']['id']
-    return None
 
 
 ##################################################################
@@ -93,9 +65,8 @@ def get_user_id():
 
 @app.route('/')
 @mobile_template('/{mobile/}index.html')
-# @login_required
+@login_required
 def index(template):
-    log()
     # TODO fetch user data
     return render_template(template)
 
@@ -103,7 +74,6 @@ def index(template):
 @app.route('/view/<calendar_id>')
 @mobile_template('{mobile/}calendar.html')
 def view(template, calendar_id):
-    log()
     # TODO check if calendar exists and if the user has permission to view it
     # TODO create template and call render
     return render_template(template)
@@ -113,7 +83,6 @@ def view(template, calendar_id):
 @mobile_template('{mobile/}template.html')
 @login_required
 def settings(template):
-    log()
     # TODO load user's settings, then render a template with the settings
     return render_template(template)
 
@@ -134,29 +103,17 @@ def save_settings():
 @login_required
 def delete_user():
     # TODO rewrite if necessary when login functionality is implemented
-    req_user = get_user_id()
+    req_user = session['user']['id']
     del_user = request.form.get('user_id', None)
     if del_user and req_user == del_user:  # ensures only the user can delete themselves
         # TODO mark user as deleted in the database
         pass
 
 
-@app.route('/delete_event', methods=['POST'])
-@login_required
-def delete_event():
-    user = get_user_id()
-    event = request.form.get('event_id', None)
-    if event:
-        # TODO
-        # Make db request to check if user has permission to delete this event
-        # Set event delete flag in db if yes
-        pass
-
-
 @app.route('/delete_calendar', methods=['POST'])
 @login_required
 def delete_cal():
-    user = get_user_id()
+    user = session['user']['id']
     cal = request.form.get('calendar_id', None)
     if cal:
         # TODO
@@ -169,8 +126,12 @@ def delete_cal():
 
 if __name__ == '__main__':
 
-    logger = setup_logging()
+    active_loggers = []
+    for log in loggers:
+        lg = setup_logging(log, '{}.log'.format(log))
+        active_loggers.append(lg)
 
     app.run()
 
-    end_logging(logger)
+    for log in active_loggers:
+        end_logging(log)
