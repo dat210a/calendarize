f7e57c22ebac082cce5390c292a57fae95d2ed38
Sierd Meijer <s.d.meijer@hotmail.com>
2017-10-25 21:04:38 +0200

    adding all missing file sinds failed push

--- app/static/js/timeline/DisplayData.js
+++ app/static/js/timeline/DisplayData.js
@@ -9,21 +9,22 @@ var detailHeight = 100,
 //load data async
 //TODO get data from SQL
 d3.queue()
-    .defer(d3.json, '/get_data')
+    .defer(d3.csv, '/static/assets/random_chart.csv')
     // .defer(d3.csv, "/static/assets/random_chart.csv")
     .await(ready);
 
-function ready(error, allData){
+function ready(error, datapoints){
     if (error){
         console.log("Can't load the data")
         return;
     }
-    console.log(allData)
-    allData.forEach(function(d, i){
-        d.color = color(i)
-    })
-    AddGroupButtons(allData)
+    console.log(datapoints)
 
+    //populate groups on the bottom menu
+    var setOfGroups = [... new Set(datapoints.map(function(d){return d.group;}))];
+    var groups = new Array(setOfGroups.length)
+    setOfGroups.forEach(function(d, i){groups[i] = {name: setOfGroups[i], color: color(i)}})
+    AddGroupButtons(groups)
 
     //sort data so its displayed from right to left
     //due to overlap
@@ -61,6 +62,7 @@ function ready(error, allData){
                             .attr('class', 'points')
                             .style('pointer-events', 'visible')
                             .style("fill", function (d) {
+                                d.color = groups.filter(function(gr){return gr.name == d.group})[0].color
                                 return d.color
                             })
                             .attr("height", radius*2)
