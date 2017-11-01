c84fea6c257790f54db3c286ccf550f6d8d20c02
Sierd Meijer <s.d.meijer@hotmail.com>
2017-11-01 11:24:49 +0100

    minor change

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -7,7 +7,7 @@
         $('.dropdown-button').dropdown({
             inDuration: 300,
             outDuration: 225,
-            hover: true, // Activate on hover
+            hover: false, // Activate on hover
             belowOrigin: true, // Displays dropdown below the button
             alignment: 'right' // Displays dropdown with edge aligned to the left of button
           }
